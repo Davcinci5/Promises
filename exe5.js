1602c92c44f8f39c4e56bc5d7727a8010af78e12
Diego <diegoa.velascoc@gmail.com>
2020-07-31 14:31:14 -0700

    exercises 5 fixed

--- exe5.js
+++ exe5.js
@@ -1,35 +1,31 @@
 //Write a function that will reverse N sized blocks of an array.
 
 function reverseBlocks(arr, blockSize){
+
     if(arr.length === 0 || blockSize <= 1) return arr;
+
     let multiplyerNextStar = 0;
+    let length = arr.length - 1;
     let start = multiplyerNextStar * blockSize,
-        end =  start + (blockSize - 1); 
-        while(end <= arr.length -1){
+        end = blockSize > length ? length:  start + (blockSize - 1), 
+        temp;
+
+        while(start < length){
             while (start < end) 
             { 
-               [arr[start],arr[end]] = [arr[end],arr[start]];
-                start++; 
-                end--; 
+                temp = arr[start];
+                arr[start++] = arr[end];
+                arr[end--] = temp;
             }
-            start = ++multiplyerNextStar * blockSize,
-            end =  start + (blockSize - 1); 
+                start = ++multiplyerNextStar * blockSize,
+                end =  start + (blockSize - 1); 
+            if(end > length) end = length; 
         }
 
-        end = arr.length-1;
-
-        while(start < end){
-            [arr[start],arr[end]] = [arr[end],arr[start]];
-             start++;
-             end--;
-        }
         return arr;
 }
-const arr = [0,1,2,3,4,5,6,7,8,9];
-const blockSize = 4;
-const data = reverseBlocks(arr, blockSize);
 
 
-// module.exports = reverseBlocks;
+ module.exports = reverseBlocks;
 
 
