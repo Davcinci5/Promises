328fc1dd9464afa299a89a4bfa04e5dc8e8e807f
Diego <diegoa.velascoc@gmail.com>
2020-07-31 13:01:33 -0700

    exercises 4.1 fixed

--- exe4.js
+++ exe4.js
@@ -5,16 +5,8 @@ function mergeArrays(largeA,smallA) {
       pointer_large = largeA.length - smallA.length - 1;
 
       for(let i = (largeA.length - 1); i>=0; i--){
-        if(largeA[pointer_large]>smallA[pointer_small] || pointer_small < 0){
-          largeA[i] = largeA[pointer_large];
-          pointer_large--;
-        }else{
-          largeA[i] = smallA[pointer_small];
-          pointer_small--;
-        }
+        largeA[i] = largeA[pointer_large] > smallA[pointer_small] ||
+                     pointer_small < 0 ? largeA[pointer_large--] : smallA[pointer_small--];
       }
 }
-
-
-
 module.exports = mergeArrays;
